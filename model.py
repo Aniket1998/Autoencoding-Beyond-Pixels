690d7f258e2423195a02e74ca2bada040574d074
yatin dandi <yatind@iitk.ac.in>
2018-09-30 21:28:07 +0530

    encoder and decoder architecture

--- model.py
+++ model.py
@@ -8,11 +8,11 @@ __all__ = ['Encoder', 'Decoder', 'Discriminator']
 class Encoder(nn.Module):
     def __init__(self, nonlinearity=None):
         nl = nn.ReLU() if nonlinearity is not None else nonlinearity
-        self.conv = nn.Sequential(nn.Conv2d(3, 64, 5, ),
+        self.conv = nn.Sequential(nn.Conv2d(3, 64, 5,stride = 2,padding = 2),
                                   nn.BatchNorm2d(64), nl,
-                                  nn.Conv2d(64, 128, 5),
+                                  nn.Conv2d(64, 128, 5,stride = 2,padding = 2),
                                   nn.BatchNorm2d(128), nl,
-                                  nn.Conv2d(128, 256, 5),
+                                  nn.Conv2d(128, 256, 5,stride = 2,padding = 2),
                                   nn.BatchNorm2d(256), nl)
         self.mean_fc = nn.Sequential(nn.Linear(8 * 8 * 256, 2048),
                                      nn.BatchNorm1d(2048), nl)
@@ -33,13 +33,13 @@ class Decoder(nn.Module):
         self.fc = nn.Sequential(nn.Linear(2048, 8 * 8 * 256),
                                 nn.BatchNorm1d(8 * 8 * 256), nl)
 
-        self.conv = nn.Sequential(nn.ConvTranspose2d(256, 256, 5,),
+        self.conv = nn.Sequential(nn.ConvTranspose2d(256, 256, 5,stride = 2,padding = 2,output_padding = 1),
                                   nn.BatchNorm2d(256), nl,
-                                  nn.ConvTranspose2d(256, 128, 5,),
+                                  nn.ConvTranspose2d(256, 128, 5,stride = 2,padding = 2,output_padding = 1),
                                   nn.BatchNorm2d(128), nl,
-                                  nn.ConvTranspose2d(128, 32, 5,),
+                                  nn.ConvTranspose2d(128, 32, 5,stride = 2,padding = 2,output_padding = 1),
                                   nn.BatchNorm2d(32), nl,
-                                  nn.ConvTranspose2d(32, 3, 5,), nn.Tanh())
+                                  nn.ConvTranspose2d(32, 3, 5,stride = 2,padding = 2,output_padding = 1), nn.Tanh())
 
     def forward(self, x):
         x = self.fc(x).view(-1, 256, 8, 8)
